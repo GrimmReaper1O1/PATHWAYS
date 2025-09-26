6fccdedd7c7efb20e44c58c30c65d4ee3b5cdf6f
GrimmReaper1O1 <152280801+GrimmReaper1O1@users.noreply.github.com>
2025-09-26 16:52:53 -0400

    Add files via upload

---
+++ PATHWAYS PUBLIC VERSION 1.2 WORKING WITHOUT COMPLETED CSS/html/SCRIPT/index.js
@@ -0,0 +1,18 @@
+let removeSplash = () => {
+    let el = document.getElementById('mainSplashScreen');
+    let el2 = document.getElementById('indexText');
+    el2.classList.remove('hidden');
+    el.classList.add('hidden');
+}
+
+let addSplash = () => {
+    let el = document.getElementById('mainSplashScreen');
+    let el2 = document.getElementById('indexText');
+    el2.classList.add('hidden');
+    el.classList.remove('hidden');
+
+}
+let blank = [];
+ sessionStorage.setItem('path', JSON.stringify(blank));
+    sessionStorage.setItem('classP', JSON.stringify(blank));
+    sessionStorage.setItem('fPath', JSON.stringify(blank));
